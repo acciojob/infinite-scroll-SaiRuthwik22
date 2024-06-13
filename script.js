d3309c67cd09da72d7b4f2c0a3e049fc61ec890c
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-13 23:22:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here!
-
+let x = 0
+function addElements(num) {
+	let list = document.getElementById("infi-list")
+	for (let i = 0; i < num; i++) {
+		const listItem = document.createElement('li')
+		listItem.textContent = `Item ${x+1}`
+		list.appendChild(listItem)
+		x+=1
+		
+	}
+}
+addElements(10)
+function handleScroll(){
+	let list = document.getElementById("infi-list")
+	if (list.scrollTop+list.clientHeight >= list.scrollHeight) {
+		addElements(2)
+	}
+}
